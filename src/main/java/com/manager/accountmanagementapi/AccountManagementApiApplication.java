5e17d36f913762c98c24a5046ac437fc0a196d23
Yashar Mustafayev <yasarmustafayev1998@gmail.com>
2022-12-25 07:32:30 +0100

    Initial dependencies were added to pom.xml

---
+++ src/main/java/com/manager/accountmanagementapi/AccountManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.manager.accountmanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountManagementApiApplication.class, args);
+	}
+
+}
